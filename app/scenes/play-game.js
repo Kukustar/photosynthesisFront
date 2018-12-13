58c7ffe20954231bcb26d039a51f443db4091977
Kukustar <aa.vv.bogachev@gmail.com>
2018-12-13 22:37:59 +0200

    add phoenix, connect with backend

--- app/scenes/play-game.js
+++ app/scenes/play-game.js
@@ -1,13 +1,18 @@
 import Phaser from 'phaser';
 import gameOptions from '../game-settings/game-options';
+import {Socket} from 'phoenix';
 
 export default class playGame extends Phaser.Scene {
   constructor() {
     super('PlayGame');
   }
   create() {
-    playGame.webSockets = new WebSocket('wss://echo.websocket.org/');
-    // playGame.webSockets.onopen = (evt) => playGame.sayHello(evt);
+    playGame.socket = new Socket('ws://localhost:4000/socket');
+    playGame.socket.connect();
+    playGame.channel = playGame.socket.channel('deck:main', {});
+    playGame.channel.on('new_msg', (event) => console.info(event));
+    playGame.channel.join();
+
     playGame.boardArray = [];
     for (let i = 0; i < gameOptions.boardSize.rows; i++) {
       playGame.boardArray[i] = [];
@@ -20,7 +25,7 @@ export default class playGame extends Phaser.Scene {
         image.on('pointerdown', () => playGame.addNewSprite(i, j));
 
 
-        const tile = this.add.sprite(position.x, position.y, 'tiles', 0)
+        const tile = this.add.sprite(position.x, position.y, 'tiles', 1)
             .setInteractive();
 
         tile.visible = false;
@@ -34,9 +39,9 @@ export default class playGame extends Phaser.Scene {
       }
     }
   }
-  // static sayHello(evt) {
-  //   console.info(evt);
-  // }
+  static sayHello(evt) {
+    console.info(evt);
+  }
   static getTitlePosition(row, col) {
     const posX = gameOptions.titleSpacing * (col + 1) + gameOptions.titleSize *
             (col + 0.5);
@@ -46,7 +51,7 @@ export default class playGame extends Phaser.Scene {
     return new Phaser.Geom.Point(posX, posY);
   }
   static addNewSprite(i, j) {
-    playGame.boardArray[i][j].tileValue = 1;
+    playGame.boardArray[i][j].tileValue = 3;
     playGame.boardArray[i][j].tileSprite.visible = true;
     playGame.boardArray[i][j].tileSprite.setFrame(0);
     const board = {};
@@ -57,6 +62,11 @@ export default class playGame extends Phaser.Scene {
       }
     }
     // playGame.webSockets.send(JSON.stringify(playGame.boardArray));
-    playGame.webSockets.send(JSON.stringify(board));
+
+    // playGame.webSockets.send(JSON.stringify(board));
+    console.info('before push');
+    const msg = {body: JSON.stringify(board)};
+    console.info(msg);
+    playGame.channel.push('new_msg', msg);
   }
 }
